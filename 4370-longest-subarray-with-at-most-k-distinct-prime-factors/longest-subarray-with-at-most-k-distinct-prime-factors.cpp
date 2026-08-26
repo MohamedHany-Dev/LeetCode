3f90191e8f7594f21197f14ccceb51b0cf9661a9
Mohamed Hany <73343985+MohamedHany-Dev@users.noreply.github.com>
2026-08-26 20:09:55 +0300

    Time: 155 ms (96.84%) | Memory: 308.4 MB (45.12%) - LeetSync

--- 4370-longest-subarray-with-at-most-k-distinct-prime-factors/longest-subarray-with-at-most-k-distinct-prime-factors.cpp
+++ 4370-longest-subarray-with-at-most-k-distinct-prime-factors/longest-subarray-with-at-most-k-distinct-prime-factors.cpp
@@ -1,83 +1,45 @@
 const int N = 1e5 + 5;
-
 int spf[N];
 vector<int> factors[N];
-
-void Mod_Sieve() {
-    iota(begin(spf), end(spf), 0);
-
-    for (int i = 2; i * i < N; ++i) {
-        if (spf[i] == i) {
-            for (int j = i * i; j < N; j += i) {
-                if (spf[j] == j) {
-                    spf[j] = i;
-                }
+// time n * log(n)
+void get_factors() {
+    for (int i = 2; i < N; ++i) {
+        if (factors[i].empty()) { // i is prime
+            for (int j = i; j < N; j += i) {
+                factors[j].push_back(i);
             }
         }
     }
 }
-
-void mod_factorize(int n) {
-    int m = n;
-
-    while (n != 1) {
-        int prime = spf[n];
-
-        // Add each prime only once
-        factors[m].push_back(prime);
-
-        while (n % prime == 0) {
-            n /= prime;
-        }
-    }
-}
-
 bool flag = false;
-
 class Solution {
 public:
     int longestSubarray(vector<int>& nums, int k) {
-
         if (!flag) {
-            Mod_Sieve();
+            get_factors();
             flag = true;
         }
-
-        vector<int> mp(N, 0);
-
-        int l = 0;
-        int longest = 0;
         int dis = 0;
-
-        for (int r = 0; r < nums.size(); ++r) {
-
-            // Generate prime factors if not already calculated
-            if (factors[nums[r]].empty()) {
-                mod_factorize(nums[r]);
+        vector<int> mp(N);
+        int l = 0, longest = 0;
+        int n = nums.size();
+        for (int r = 0; r < n; ++r) {
+            // add primes of new element
+            for (auto x : factors[nums[r]]) {
+                if(++mp[x] == 1) ++dis;
             }
-
-            // Add nums[r]
-            for (int prime : factors[nums[r]]) {
-                if (++mp[prime] == 1) {
-                    ++dis;
-                }
-            }
-
-            // Keep at most k distinct prime factors
+            // remove from l if distinct more than k
             while (dis > k) {
-
-                for (int prime : factors[nums[l]]) {
-                    if (--mp[prime] == 0) {
+                // remove primes of left element
+                for (auto x : factors[nums[l]]) {
+                    if (--mp[x] == 0) {
                         --dis;
                     }
                 }
-
                 ++l;
             }
-
             longest = max(longest, r - l + 1);
         }
-
         return longest;
     }
 };
